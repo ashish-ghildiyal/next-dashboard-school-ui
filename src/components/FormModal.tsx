a563e45f5a005a33ef0d43f706f896a5b4a7bfcd
Ashish <ashish.ghildiyal@xelba.io>
2025-10-28 15:58:49 +0530

    dashboard ui

---
+++ src/components/FormModal.tsx
@@ -0,0 +1,112 @@
+"use client"
+import dynamic from "next/dynamic";
+import Image from "next/image";
+import { useState } from "react";
+// USE LAZY LOADING
+const TeacherForm = dynamic(() => import("./forms/TeacherForm"), {
+  loading: () => <h1>Loading...</h1>,
+});
+const StudentForm = dynamic(() => import("./forms/StudentForm"), {
+  loading: () => <h1>Loading...</h1>,
+});
+
+const forms: {
+  [key: string]: (type: "create" | "update", data?: any) => JSX.Element;
+} = {
+  teacher: (type, data) => <TeacherForm type={type} data={data} />,
+  student: (type, data) => <StudentForm type={type} data={data} />
+};
+const FormModal = ({table ,type, data, id}:{
+    table: | "teacher"
+    | "student"
+    | "parent"
+    | "subject"
+    | "class"
+    | "lesson"
+    | "exam"
+    | "assignment"
+    | "result"
+    | "attendance"
+    | "event"
+    | "announcement";
+    type: "create" | "update" | "delete";
+    data?: any;
+    id?: number;
+}) => {
+    const [open, setOpen] = useState(false);
+    const size = type === "create" ? "w-8 h-8" : "w-7 h-7";
+    const bgColor =
+    type === "create"
+      ? "bg-agYellow"
+      : type === "update"
+      ? "bg-agSky"
+      : "bg-agPurple";
+   const Form = () => {
+     if (type === "delete" && id) {
+      return (
+        <form action="" className="p-4 flex flex-col gap-4">
+          <span className="text-center font-medium">
+            All data will be lost. Are you sure you want to delete this {table}?
+          </span>
+          <button className="bg-red-700 text-white py-2 px-4 rounded-md border-none w-max self-center">
+             Delete
+          </button>
+        </form>     
+        )   
+      }
+
+    if (type === "create" || type === "update") {
+     const renderer = forms[table];
+      if (typeof renderer === "function") {
+        return renderer(type, data);
+       }
+      return (
+        <div className="p-4 text-center">
+          Form not found for "{table}"
+        </div>
+      );
+    }
+    return <div className="p-4 text-center">Form not found!</div>;
+} 
+
+    //  return type === "delete" && id ? (
+    //   <form action="" className="p-4 flex flex-col gap-4">
+    //     <span className="text-center font-medium">
+    //       All data will be lost. Are you sure you want to delete this {table}?
+    //     </span>
+    //     <button className="bg-red-700 text-white py-2 px-4 rounded-md border-none w-max self-center">
+    //       Delete
+    //     </button>
+    //   </form>
+    //   ): type === "create" || type === "update" ? (
+    //   forms[table](type, data)
+    // ) : (
+    //   "Form not found!"
+    // );
+      
+  return (
+    <>
+      <button
+        className={`${size} flex items-center justify-center rounded-full ${bgColor}`}
+        onClick={() => setOpen(true)}
+      >
+        <Image src={`/${type}.png`} alt="" width={16} height={16} />
+      </button>
+      {open && (
+        <div className="w-screen h-screen absolute left-0 top-0 bg-black bg-opacity-60 z-50 flex items-center justify-center">
+          <div className="bg-white p-4 rounded-md relative w-[90%] md:w-[70%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%]">
+            <Form />
+            <div
+              className="absolute top-4 right-4 cursor-pointer"
+              onClick={() => setOpen(false)}
+            >
+              <Image src="/close.png" alt="" width={14} height={14} />
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default FormModal
